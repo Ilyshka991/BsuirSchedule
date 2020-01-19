c525482ba2c0f27a59a0a4ceab5028b2a4f4b447
Ilya <33559094+Ilyshka991@users.noreply.github.com>
2020-01-20 01:13:26 +0300

    Add firebase services: crashlytics, analytics (#11)
    Add firebase services: crashlytics, analytics

--- app/src/main/java/com/pechuro/bsuirschedule/App.kt
+++ app/src/main/java/com/pechuro/bsuirschedule/App.kt
@@ -2,6 +2,8 @@ package com.pechuro.bsuirschedule
 
 import android.app.Activity
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.pechuro.bsuirschedule.di.component.DaggerAppComponent
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
@@ -12,12 +14,13 @@ class App : Application(), HasActivityInjector {
     @Inject
     lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
 
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
     override fun onCreate() {
         super.onCreate()
-
-        /*if (LeakCanary.isInAnalyzerProcess(this)) return
-        LeakCanary.install(this)*/
-
         initDI()
     }
 
