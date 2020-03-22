8561a8515e93b62fc6477a4f09b6e9b0fe5b8541
Nikita Maslov <developer.nikita.maslov@gmail.com>
2020-03-22 21:39:40 +0300

    Implement GroupWeekClassesViewHolder

--- app/src/main/java/com/pechuro/bsuirschedule/feature/displayschedule/fragment/DisplayScheduleItemAdapter.kt
+++ app/src/main/java/com/pechuro/bsuirschedule/feature/displayschedule/fragment/DisplayScheduleItemAdapter.kt
@@ -9,9 +9,11 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import com.pechuro.bsuirschedule.R
 import com.pechuro.bsuirschedule.common.base.BaseViewHolder
+import com.pechuro.bsuirschedule.domain.entity.WeekNumber
 import com.pechuro.bsuirschedule.feature.displayschedule.data.DisplayScheduleItem
 import kotlinx.android.synthetic.main.item_display_employee_day_classes.*
 import kotlinx.android.synthetic.main.item_display_group_day_classes.*
+import kotlinx.android.synthetic.main.item_display_group_week_classes.*
 
 val DIFF_CALLBACK = object : DiffUtil.ItemCallback<DisplayScheduleItem>() {
 
@@ -29,6 +31,7 @@ val DIFF_CALLBACK = object : DiffUtil.ItemCallback<DisplayScheduleItem>() {
 
 
 @LayoutRes private const val GROUP_DAY_CLASSES_VIEW_TYPE = R.layout.item_display_group_day_classes
+@LayoutRes private const val GROUP_WEEK_CLASSES_VIEW_TYPE = R.layout.item_display_group_week_classes
 @LayoutRes private const val EMPLOYEE_DAY_CLASSES_VIEW_TYPE = R.layout.item_display_employee_day_classes
 
 
@@ -36,6 +39,7 @@ class DisplayScheduleItemAdapter : ListAdapter<DisplayScheduleItem, BaseViewHold
 
     override fun getItemViewType(position: Int): Int = when (getItem(position)) {
         is DisplayScheduleItem.GroupDayClasses -> GROUP_DAY_CLASSES_VIEW_TYPE
+        is DisplayScheduleItem.GroupWeekClasses -> GROUP_WEEK_CLASSES_VIEW_TYPE
         is DisplayScheduleItem.EmployeeDayClasses -> EMPLOYEE_DAY_CLASSES_VIEW_TYPE
         else -> throw IllegalStateException()
     }
@@ -45,6 +49,7 @@ class DisplayScheduleItemAdapter : ListAdapter<DisplayScheduleItem, BaseViewHold
         val view = layoutInflater.inflate(viewType, parent, false)
         return when (viewType) {
             GROUP_DAY_CLASSES_VIEW_TYPE -> GroupDayClassesViewHolder(view)
+            GROUP_WEEK_CLASSES_VIEW_TYPE -> GroupWeekClassesViewHolder(view)
             EMPLOYEE_DAY_CLASSES_VIEW_TYPE -> EmployeeDayClassesViewHolder(view)
             else -> throw IllegalStateException()
         }
@@ -77,6 +82,48 @@ class DisplayScheduleItemAdapter : ListAdapter<DisplayScheduleItem, BaseViewHold
                 displayGroupDayClassesStartTime.text = startTime
                 displayGroupDayClassesEndTime.text = endTime
                 displayGroupDayClassesNotes.apply {
+                    if (note.isNotEmpty() && note.isNotBlank()) {
+                        text = note
+                        visibility = View.VISIBLE
+                    } else {
+                        visibility = View.GONE
+                    }
+                }
+            }
+        }
+    }
+
+    private inner class GroupWeekClassesViewHolder(view: View) :
+            BaseViewHolder<DisplayScheduleItem.GroupWeekClasses>(view) {
+
+        override fun onBind(data: DisplayScheduleItem.GroupWeekClasses) {
+            with(data.scheduleItem) {
+                displayGroupWeekClassesLessonType.text = lessonType
+                displayGroupWeekClassesTitle.text = subject
+                displayGroupWeekClassesAuditories.text = auditories.joinToString(separator = ",") { it.name }
+                displayGroupWeekClassesSubGroup.apply {
+                    if (subgroupNumber != 0) {
+                        text = context.getString(R.string.display_schedule_item_subgroup, subgroupNumber)
+                        visibility = View.VISIBLE
+                    } else {
+                        visibility = View.GONE
+                    }
+                }
+                displayGroupWeekClassesWeekNumbers.apply {
+                    if (data.weekNumbers.size == WeekNumber.TOTAL_COUNT) {
+                        visibility = View.GONE
+                    } else {
+                        text = context.getString(
+                                R.string.display_schedule_item_week_numbers,
+                                data.weekNumbers.joinToString(separator = ",") { it.index.toString() }
+                        )
+                        visibility = View.VISIBLE
+                    }
+                }
+                displayGroupWeekClassesEmployees.text = employees.joinToString(separator = ",") { it.abbreviation }
+                displayGroupWeekClassesStartTime.text = startTime
+                displayGroupWeekClassesEndTime.text = endTime
+                displayGroupWeekClassesNotes.apply {
                     if (note.isNotEmpty() && note.isNotBlank()) {
                         text = note
                         visibility = View.VISIBLE
