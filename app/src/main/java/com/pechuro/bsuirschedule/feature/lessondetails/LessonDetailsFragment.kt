c2ac42ebd1d43be0043dd98aa3e21810e9436086
EvGem <evg200003@gmail.com>
2020-03-30 04:15:24 +0300

    Add base presentation

--- app/src/main/java/com/pechuro/bsuirschedule/feature/lessondetails/LessonDetailsFragment.kt
+++ app/src/main/java/com/pechuro/bsuirschedule/feature/lessondetails/LessonDetailsFragment.kt
@@ -1,13 +1,41 @@
 package com.pechuro.bsuirschedule.feature.lessondetails
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.pechuro.bsuirschedule.R
 import com.pechuro.bsuirschedule.common.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_lesson_details.*
 
 class LessonDetailsFragment : BaseFragment() {
 
     private val viewModel by lazy(LazyThreadSafetyMode.NONE) {
-        initViewModel(LessonDetailsViewModel::class)
+        initViewModel(LessonDetailsViewModel::class).also {
+            it.setLesson(args.lesson)
+        }
     }
 
+    private val args: LessonDetailsFragmentArgs by navArgs()
+
     override val layoutId: Int get() = R.layout.fragment_lesson_details
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        lessonDetailsName.text = viewModel.subject
+        lessonDetailsTime.text = viewModel.time
+        Glide.with(this)
+                .load(viewModel.employeePhotoUrl)
+                .placeholder(R.drawable.employee_placeholder)
+                .error(R.drawable.employee_placeholder)
+                .circleCrop()
+                .into(lessonDetailsPhoto)
+        lessonDetailsFullName.text = viewModel.employeeFullName
+        lessonDetailsWeeks.text = viewModel.weeks
+        lessonDetailsAuditory.text = viewModel.auditory
+    }
 }
--- app/src/main/java/com/pechuro/bsuirschedule/feature/lessondetails/LessonDetailsViewModel.kt
+++ app/src/main/java/com/pechuro/bsuirschedule/feature/lessondetails/LessonDetailsViewModel.kt
@@ -1,6 +1,32 @@
 package com.pechuro.bsuirschedule.feature.lessondetails
 
 import com.pechuro.bsuirschedule.common.base.BaseViewModel
+import com.pechuro.bsuirschedule.domain.entity.ILesson
 import javax.inject.Inject
 
-class LessonDetailsViewModel @Inject constructor() : BaseViewModel()
+class LessonDetailsViewModel @Inject constructor() : BaseViewModel() {
+
+    private lateinit var lesson: ILesson
+
+    lateinit var subject: String
+
+    lateinit var time: String
+
+    lateinit var employeePhotoUrl: String
+
+    lateinit var employeeFullName: String
+
+    lateinit var weeks: String
+
+    lateinit var auditory: String
+
+    fun setLesson(lesson: ILesson) {
+        this.lesson = lesson
+        subject = lesson.subject
+        time = with(lesson) { "$startTime - $endTime" }
+        employeePhotoUrl = "https://www.grassrootinstitute.org/wp-content/uploads/2013/04/00409335.jpg" // TODO
+        employeeFullName = "Asian teacher" // TODO
+        weeks = "Weeks: ${lesson.weekNumber.index + 1}" // TODO
+        auditory = lesson.auditories.firstOrNull()?.run { "$name-${building.name}" } ?: "" //TODO
+    }
+}
