25c250bb0ccc87e1ff46b1d31bfd2e5c383113ef
EvGem <evg200003@gmail.com>
2020-04-21 02:43:48 +0300

    Display base employee info

--- app/src/main/java/com/pechuro/bsuirschedule/feature/lessondetails/LessonDetailsAdapter.kt
+++ app/src/main/java/com/pechuro/bsuirschedule/feature/lessondetails/LessonDetailsAdapter.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import androidx.core.text.bold
 import androidx.core.text.toSpannable
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayoutMediator
 import com.pechuro.bsuirschedule.R
 import com.pechuro.bsuirschedule.domain.entity.Auditory
 import com.pechuro.bsuirschedule.domain.entity.Lesson
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_employee_details.*
 import kotlinx.android.synthetic.main.item_lesson_details_header.*
 import kotlinx.android.synthetic.main.item_lesson_details_location.*
 
@@ -32,7 +35,7 @@ class LessonDetailsAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
         return when (viewType) {
-            HEADER_VIEW_TYPE -> HeaderViewHolder(view)
+            HEADER_VIEW_TYPE -> HeaderViewHolder(view).also { initHeader(it) }
             LOCATION_VIEW_TYPE -> LocationViewHolder(view)
             else -> throw IllegalArgumentException()
         }
@@ -81,6 +84,24 @@ class LessonDetailsAdapter(
         lessonDetailsLocationsTitle.visibility =
                 if (lesson.auditories.isNotEmpty()) View.VISIBLE
                 else View.GONE
+
+        return@with when (lesson) {
+            is Lesson.GroupLesson -> onBindGroupHeader(holder, lesson)
+            is Lesson.EmployeeLesson -> onBindEmployeeHeader(holder, lesson)
+        }
+    }
+
+    private fun onBindEmployeeHeader(holder: HeaderViewHolder, employeeLesson: Lesson.EmployeeLesson) {
+        TODO()
+    }
+
+    private fun onBindGroupHeader(holder: HeaderViewHolder, groupLesson: Lesson.GroupLesson) {
+        holder.lessonDetailsInfoViewPager.visibility =
+                if (groupLesson.employees.isNotEmpty()) View.VISIBLE
+                else View.GONE
+        holder.lessonDetailsInfoTabLayout.visibility =
+                if (groupLesson.employees.size > 1) View.VISIBLE
+                else View.GONE
     }
 
     private fun getWeeksText(context: Context): CharSequence {
@@ -93,7 +114,48 @@ class LessonDetailsAdapter(
         return builder.toSpannable()
     }
 
+    private fun initHeader(holder: HeaderViewHolder) = when (lesson) {
+        is Lesson.GroupLesson -> initGroupHeader(holder, lesson)
+        is Lesson.EmployeeLesson -> initEmployeeHeader(holder, lesson)
+    }
+
+    private fun initEmployeeHeader(holder: HeaderViewHolder, employeeLesson: Lesson.EmployeeLesson) {
+        TODO("Not yet implemented")
+    }
+
+    private fun initGroupHeader(holder: HeaderViewHolder, groupLesson: Lesson.GroupLesson) {
+        holder.lessonDetailsInfoViewPager.adapter = EmployeeAdapter(groupLesson)
+        TabLayoutMediator(holder.lessonDetailsInfoTabLayout, holder.lessonDetailsInfoViewPager) { _, _ -> }.attach()
+    }
+
     private class HeaderViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
 
     private class LocationViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
+
+    private class EmployeeAdapter(
+            private val groupLesson: Lesson.GroupLesson
+    ) : RecyclerView.Adapter<EmployeeViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_employee_details, parent, false)
+            return EmployeeViewHolder(view)
+        }
+
+        override fun getItemCount(): Int = groupLesson.employees.size
+
+        override fun onBindViewHolder(holder: EmployeeViewHolder, position: Int) {
+            val employee = groupLesson.employees[position]
+            holder.employeeDetailsFullName.text = with(employee) {
+                "$firstName $middleName $lastName"
+            }
+            Glide.with(holder.itemView)
+                    .load(employee.photoLink)
+                    .placeholder(R.drawable.employee_placeholder)
+                    .error(R.drawable.employee_placeholder)
+                    .circleCrop()
+                    .into(holder.employeeDetailsPhoto)
+        }
+    }
+
+    private class EmployeeViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
 }
