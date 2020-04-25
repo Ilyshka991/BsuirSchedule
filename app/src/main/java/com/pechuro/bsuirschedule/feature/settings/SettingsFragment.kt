b4147e99854a6c9441985032e487559f628efcee
Ilya Pechuro <ilyapechuro991@gmail.com>
2020-04-26 00:56:54 +0300

    Add info loading to settings

--- app/src/main/java/com/pechuro/bsuirschedule/feature/settings/SettingsFragment.kt
+++ app/src/main/java/com/pechuro/bsuirschedule/feature/settings/SettingsFragment.kt
@@ -5,7 +5,10 @@ import android.view.View
 import com.pechuro.bsuirschedule.BuildConfig
 import com.pechuro.bsuirschedule.R
 import com.pechuro.bsuirschedule.common.base.BaseFragment
+import com.pechuro.bsuirschedule.ext.nonNull
+import com.pechuro.bsuirschedule.ext.observe
 import com.pechuro.bsuirschedule.ext.setSafeClickListener
+import com.pechuro.bsuirschedule.ext.setVisibleWithAlpha
 import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : BaseFragment() {
@@ -26,8 +29,11 @@ class SettingsFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initView()
+        observeData()
         setViewListeners()
     }
+
+    override fun onBackPressed() = viewModel.setNormalState()
 
     private fun initView() {
         settingsToolbar.apply {
@@ -39,12 +45,41 @@ class SettingsFragment : BaseFragment() {
         settingsVersionText.setMessage(BuildConfig.VERSION_NAME)
     }
 
+    private fun observeData() {
+        viewModel.stateData.nonNull().observe(viewLifecycleOwner) {
+            updateLayoutState(it)
+        }
+    }
+
+    private fun updateLayoutState(state: SettingsViewModel.State) {
+        when (state) {
+            is SettingsViewModel.State.Idle -> {
+                settingActionParentView.setVisibleWithAlpha(true)
+                settingsInfoLoadLoaderView.setVisibleWithAlpha(false)
+                settingsInfoLoadErrorParentView.setVisibleWithAlpha(false)
+            }
+            is SettingsViewModel.State.Loading -> {
+                settingActionParentView.setVisibleWithAlpha(false)
+                settingsInfoLoadLoaderView.setVisibleWithAlpha(true)
+                settingsInfoLoadErrorParentView.setVisibleWithAlpha(false)
+            }
+            is SettingsViewModel.State.Error -> {
+                settingActionParentView.setVisibleWithAlpha(false)
+                settingsInfoLoadLoaderView.setVisibleWithAlpha(false)
+                settingsInfoLoadErrorParentView.setVisibleWithAlpha(true)
+            }
+        }
+    }
+
     private fun setViewListeners() {
         settingsThemeButton.setSafeClickListener {
 
         }
         settingsUpdateInfoButton.setSafeClickListener {
-
+            viewModel.updateInfo()
+        }
+        settingsInfoLoadErrorButton.setSafeClickListener {
+            viewModel.updateInfo()
         }
         settingsRateAppButton.setSafeClickListener {
 
--- app/src/main/java/com/pechuro/bsuirschedule/feature/settings/SettingsViewModel.kt
+++ app/src/main/java/com/pechuro/bsuirschedule/feature/settings/SettingsViewModel.kt
@@ -1,9 +1,47 @@
 package com.pechuro.bsuirschedule.feature.settings
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.pechuro.bsuirschedule.common.base.BaseViewModel
+import com.pechuro.bsuirschedule.domain.common.BaseInteractor
+import com.pechuro.bsuirschedule.domain.common.fold
+import com.pechuro.bsuirschedule.domain.exception.DataSourceException
+import com.pechuro.bsuirschedule.domain.interactor.LoadInfo
 import javax.inject.Inject
 
-class SettingsViewModel @Inject constructor() : BaseViewModel() {
+class SettingsViewModel @Inject constructor(
+        private val loadInfo: LoadInfo
+) : BaseViewModel() {
 
+    private val _stateData = MutableLiveData<State>(State.Idle)
+    val stateData: LiveData<State>
+        get() = _stateData
 
+    fun updateInfo() {
+        launchCoroutine {
+            _stateData.value = State.Loading
+            loadInfo.execute(BaseInteractor.NoParams).fold(
+                    onSuccess = { _stateData.value = State.Idle },
+                    onFailure = { exception ->
+                        if (exception is DataSourceException.CancellationException) return@fold
+                        _stateData.value = State.Error
+                    }
+            )
+        }
+    }
+
+    fun setNormalState(): Boolean {
+        val isInNormalState = _stateData.value == State.Idle
+        if (!isInNormalState) {
+            cancelAllJobs()
+            _stateData.value = State.Idle
+        }
+        return !isInNormalState
+    }
+
+    sealed class State {
+        object Idle : State()
+        object Loading : State()
+        object Error : State()
+    }
 }
