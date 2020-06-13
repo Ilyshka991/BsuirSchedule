0abac9b401462af0c7457c6812e3b20c02a588e3
Ilya Pechuro <ilyapechuro991@gmail.com>
2020-06-13 21:49:44 +0300

    Add colors for loader

--- app/src/main/java/com/pechuro/bsuirschedule/widget/LoaderView.kt
+++ app/src/main/java/com/pechuro/bsuirschedule/widget/LoaderView.kt
@@ -1,12 +1,18 @@
 package com.pechuro.bsuirschedule.widget
 
 import android.content.Context
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.use
 import androidx.core.view.isVisible
+import com.airbnb.lottie.LottieProperty
+import com.airbnb.lottie.model.KeyPath
 import com.pechuro.bsuirschedule.R
+import com.pechuro.bsuirschedule.ext.colorFromAttr
 import kotlinx.android.synthetic.main.view_loader.view.*
+
 
 class LoaderView @JvmOverloads constructor(
         context: Context,
@@ -16,6 +22,20 @@ class LoaderView @JvmOverloads constructor(
 
     init {
         inflate(context, R.layout.view_loader, this)
+
+        val foregroundColor = context.colorFromAttr(R.attr.textDefaultColor)
+        val foregroundColorFilter = PorterDuffColorFilter(foregroundColor, PorterDuff.Mode.SRC_ATOP)
+        loaderAnimationView.addValueCallback(
+                KeyPath("foreground", "**"),
+                LottieProperty.COLOR_FILTER
+        ) { foregroundColorFilter }
+
+        val backgroundColor = context.colorFromAttr(R.attr.rippleColor)
+        val backgroundColorFilter = PorterDuffColorFilter(backgroundColor, PorterDuff.Mode.SRC_ATOP)
+        loaderAnimationView.addValueCallback(
+                KeyPath("background", "**"),
+                LottieProperty.COLOR_FILTER
+        ) { backgroundColorFilter }
 
         context.obtainStyledAttributes(attrs, R.styleable.LoaderView).use {
             val message = it.getString(R.styleable.LoaderView_message) ?: ""
